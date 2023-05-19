69694e14f5b750ed1059249139ac68c1ff1c60da
linaramdez <119882929+linaramdez@users.noreply.github.com>
2023-05-19 11:33:09 +0200

    Add files via upload

---
+++ M02/battleMarti.sql
@@ -0,0 +1,5 @@
+/*
+-- Query: select * from battle order by player_id desc limit 1
+-- Date: 2023-05-18 20:34
+*/
+INSERT INTO `` (`battle_id`,`player_id`,`warrior_id`,`warrior_weapon_id`,`opponent_id`,`opponent_weapon_id`,`injuries_caused`,`injuries_suffered`,`battle_points`) VALUES (13,21,5,3,6,2,60,27,21);
